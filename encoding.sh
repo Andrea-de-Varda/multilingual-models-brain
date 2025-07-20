4e6f40cf8e3fb4659a06db1928f8ef85251c8bc5
Andrea de Varda <devar_ag@evnode.ib.cluster>
2025-07-20 10:16:39 -0400

    monolingual results

---
+++ encoding.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p evlab
+#SBATCH --mem=20G
+#SBATCH -c 1
+#SBATCH -t 30:00:00
+#SBATCH --output=output_%j.out
+
+source /cm/shared/openmind/anaconda/3-2021.05/etc/profile.d/conda.sh
+conda activate reasoning-models
+
+# python fit_encoding.py --mode within
+python fit_encoding.py --mode across
+# python fit_encoding.py --mode RH
+# python fit_encoding.py --mode MD
