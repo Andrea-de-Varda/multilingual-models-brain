893403b7e389b8a8007cd782b6f9e4b3ccd84719
Andrea de Varda <andrea.devarda@gmail.com>
2025-07-26 14:46:27 -0400

    results native-across

--- encoding.sh
+++ encoding.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p evlab
-#SBATCH --mem=10G
+#SBATCH --mem=6G
 #SBATCH -c 1
 #SBATCH -t 40:00:00
 #SBATCH --output=output_%j.out
@@ -8,9 +8,9 @@
 source /cm/shared/openmind/anaconda/3-2021.05/etc/profile.d/conda.sh
 conda activate reasoning-models
 
-python fit_encoding.py --mode within
-python fit_encoding.py --mode across
-python fit_encoding.py --mode RH
-python fit_encoding.py --mode MD
+# python fit_encoding.py --mode within
+# python fit_encoding.py --mode across
+# python fit_encoding.py --mode RH
+# python fit_encoding.py --mode MD
 python fit_encoding.py --mode native-within
 python fit_encoding.py --mode native-across
