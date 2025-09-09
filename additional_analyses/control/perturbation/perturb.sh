6bd71d34ccf7c12952771f168b614e91ee75d5aa
Andrea de Varda <andrea.devarda@gmail.com>
2025-09-09 12:01:15 -0400

    cntrol perturbation

--- additional_analyses/control/perturbation/perturb.sh
+++ additional_analyses/control/perturbation/perturb.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 #SBATCH -p evlab
+#SBATCH --qos=evlab
 #SBATCH --mem=23G
-#SBATCH --gres=shard:a100:1
+#SBATCH --gres=gpu:1
 #SBATCH -c 1
 #SBATCH -t 10:00:00
 #SBATCH --output=perturb_output_%j.out
+#SBATCH --error=perturb_error_%j.err
+#SBATCH --chdir=/om2/user/devar_ag/multilingual-models-brain/additional_analyses/control
 
+# Load conda
 source /cm/shared/openmind/anaconda/3-2021.05/etc/profile.d/conda.sh
 conda activate reasoning-models
-cd multilingual-models-brain/additional_analyses/control
 
-python perturbation/perturbation_encoding_DEF.py
+echo "Running on $(hostname)"
+echo "PWD: $(pwd)"
+which python
+python --version
 
+python perturbation/perturbation_encoding.py
